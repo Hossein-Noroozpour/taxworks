af0cb1f5e73c0105293435de0fdf96e9f2a87f45
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2018-05-21 15:06:20 +0430

    xsd generator changed, now query p1 for w091 has gto

--- xsd-generator/src/main.rs
+++ xsd-generator/src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::Write;
 
 fn convert_to_string(i: i32) -> String {
     if i == 0 {
@@ -5,33 +7,36 @@ fn convert_to_string(i: i32) -> String {
     } else if i < 0 {
         panic!("Unacceptable number");
     }
-    const BASE: i32 = 52i32;
-    const INDC: i32 = 26i32;
+    const BASE: i32 = 26i32;
     let mut result = String::new();
     let mut dom = i;
     loop {
         if dom < 1 { break; }
         let rem = dom % BASE;
-        if rem < INDC {
-            result += &((rem + 'A' as i32) as u8 as char).to_string();
-        } else {
-            result += &(((rem - 26) + 'a' as i32) as u8 as char).to_string();
-        }
+        result += &((rem + 'A' as i32) as u8 as char).to_string();
         dom /= BASE;
     }
     result
 }
 
 fn main() {
-    println!("<xs:schema attributeFormDefault=\"unqualified\" elementFormDefault=\"qualified\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\">");
-    println!("  <xs:element name=\"content\">");
-    println!("    <xs:complexType>");
-    println!("      <xs:sequence>");
+    let mut file = File::create("1.xsd").expect("Failed to open XSD file.");
+    write!(&mut file, "<xs:schema attributeFormDefault=\"unqualified\" elementFormDefault=\"qualified\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\">\n").unwrap();
+    write!(&mut file, "  <xs:element name=\"content\">\n").unwrap();
+    write!(&mut file, "    <xs:complexType>\n").unwrap();
+    write!(&mut file, "      <xs:all>\n").unwrap();
     for i in 1..103 {
-        println!("        <xs:element type=\"xs:string\" name=\"{}\"/><!--{}-->", convert_to_string(i), i);
+        write!(&mut file, "        <xs:element type=\"xs:string\" name=\"{}\" minOccurs=\"0\" maxOccurs=\"1\"/><!--{}-->\n", convert_to_string(i), i).unwrap();
     }
-    println!("      </xs:sequence>");
-    println!("    </xs:complexType>");
-    println!("  </xs:element>");
-    println!("</xs:schema>");
+    write!(&mut file, "      </xs:all>\n").unwrap();
+    write!(&mut file, "    </xs:complexType>\n").unwrap();
+    write!(&mut file, "  </xs:element>\n").unwrap();
+    write!(&mut file, "</xs:schema>\n").unwrap();
+    let mut file = File::create("1.xml").expect("Failed to open XML file.");
+    write!(&mut file, "<content>\n").unwrap();
+    for i in 1..103 {
+        let e = convert_to_string(i);
+        write!(&mut file, "  <{}>{}-{}<{}><!--{}-->\n", e, i, e, e, i).unwrap();
+    }
+    write!(&mut file, "</content>\n").unwrap();
 }
